48150a0b749e664fc8b5e2ee8a9c477c52c026fe
Willian Mascimiano <willian@wsa.com.br>
2024-02-03 19:20:03 -0300

    feat:  rabbitmq initialize

---
+++ src/main/java/com/agroeti/connections/RabbitMQConnection.java
@@ -0,0 +1,16 @@
+package com.agroeti.connections;
+
+import com.agroeti.constants.RabbitMQConstants;
+import jakarta.annotation.PostConstruct;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+ import org.springframework.stereotype.Component;
+
+
+@Component
+public class RabbitMQConnection {
+
+
+}
---
+++ src/main/java/com/agroeti/constants/RabbitMQConstants.java
@@ -0,0 +1,7 @@
+package com.agroeti.constants;
+
+public class RabbitMQConstants {
+
+    public static final String FILA_1 = "FILA1";
+    public static final String FILA_2 = "FILA2";
+}
